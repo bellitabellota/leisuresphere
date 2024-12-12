be6b56aad58a8e961b5e240e1ce2d4fd566ba609
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-12 10:57:02 +0000

    Apply unique constraint on database level

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_11_115136) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false, unique: true
-    t.string "email", default: "", null: false, unique: true
+    t.string "name", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_11_115136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
