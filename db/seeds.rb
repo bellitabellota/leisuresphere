f31a37f4999010769d0450c09ba547bd50dcceac
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-12 12:43:31 +0000

    Add some users to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.create(name: "John", email: "john@mail.com", password: "johnjohn")
+User.create(name: "Alice", email: "alice@mail.com", password: "alicealice")
+User.create(name: "Bruce", email: "bruce@mail.com", password: "brucebruce")
+User.create(name: "Suzanne", email: "suzanne@mail.com", password: "suzannesuzanne")
+User.create(name: "Vincente", email: "vincente@mail.com", password: "vincentevincente")
