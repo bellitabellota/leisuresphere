f5a1eb57ede7ca0af4c1b30b047070d7bb071594
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-12 17:25:28 +0000

    Generate and migrate Follow model and add associations with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
+  has_many :followers, through: :follower_follows, source: :follower
+
+  has_many :follows, foreign_key: :follower_id
+  has_many :followees, through: :follows, source: :followee
+
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
