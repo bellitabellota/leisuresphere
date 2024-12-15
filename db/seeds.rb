aada26dda4114025194ba1bc5380c3ab9ac7ce33
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-15 10:42:06 +0000

    Add sample posts to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,15 @@
 #   end
 
 
-User.find_by(name: "John") || User.create(name: "John", email: "john@mail.com", password: "johnjohn")
-User.find_by(name: "Alice") || User.create(name: "Alice", email: "alice@mail.com", password: "alicealice")
-User.find_by(name: "Bruce") || User.create(name: "Bruce", email: "bruce@mail.com", password: "brucebruce")
-User.find_by(name: "Suzanne") || User.create(name: "Suzanne", email: "suzanne@mail.com", password: "suzannesuzanne")
-User.find_by(name: "Vincente") || User.create(name: "Vincente", email: "vincente@mail.com", password: "vincentevincente")
+j = User.find_by(name: "John") || User.create(name: "John", email: "john@mail.com", password: "johnjohn")
+a = User.find_by(name: "Alice") || User.create(name: "Alice", email: "alice@mail.com", password: "alicealice")
+b = User.find_by(name: "Bruce") || User.create(name: "Bruce", email: "bruce@mail.com", password: "brucebruce")
+s = User.find_by(name: "Suzanne") || User.create(name: "Suzanne", email: "suzanne@mail.com", password: "suzannesuzanne")
+v = User.find_by(name: "Vincente") || User.create(name: "Vincente", email: "vincente@mail.com", password: "vincentevincente")
+
+v.posts.find_or_create_by!(body: "This is Vincente's first post.")
+v.posts.find_or_create_by!(body: "This is Vincente's second post.")
+s.posts.find_or_create_by!(body: "Here are Suzanne's highlights of the day.")
+b.posts.find_or_create_by!(body: "Bruce has special plans for this week. He is gonna try something new.")
+a.posts.find_or_create_by!(body: "Alice has to be careful to not fall into the rabbit hole.")
+j.posts.find_or_create_by!(body: "The origin of the name John is in contrast to what one may assume quite pecuiliar.")
