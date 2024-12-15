423519611c6f42db6fb083cdd6ce8eedf95603d0
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-15 11:49:24 +0000

    Add the option to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = current_user.posts.build()
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      # flash message?
+      redirect_to root_path
+    else
+      # flash_message
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
