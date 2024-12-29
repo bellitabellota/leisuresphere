b3a47db58cd16a5d2df1b382ce47829a4589560a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-29 14:52:33 +0000

    Remove unnecessary content

--- spec/system/likes/like_a_post_spec.rb
+++ spec/system/likes/like_a_post_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe "Like a post", type: :system do
   context "when current_user likes a post" do
     it "the like count increases by 1" do
       user = FactoryBot.create(:user)
-      # another_user = FactoryBot.create(:second_user)
-      # FactoryBot.create(:follow, followee_id: another_user.id, follower_id: user.id)
       FactoryBot.create(:post, author_id: user.id)
 
       login_as user
