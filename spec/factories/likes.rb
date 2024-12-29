3c1f2744adcd577dfee7184d1cc63a450e397950
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-29 14:48:59 +0000

    Update Like Factory and User-Like-post association tests

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :like do
     association :liker, factory: :user
-    association :liked_post, factory: :post
+    association :likeable, factory: :post
   end
 end
--- spec/models/user_like_post_associations_spec.rb
+++ spec/models/user_like_post_associations_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "User-Like-Post associations", type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:post) { FactoryBot.create(:post) }
-  let!(:like) { FactoryBot.create(:like, liker_id: user.id, liked_post_id: post.id) }
+  let!(:like) { FactoryBot.create(:like, liker_id: user.id, likeable_id: post.id, likeable_type: "Post") }
 
   context "when user likes a post" do
     it "user.liked_posts returns a collection that includes the post" do
+      p like
       expect(user.liked_posts).to include(post)
     end
 
@@ -19,6 +20,7 @@ RSpec.describe "User-Like-Post associations", type: :model do
     it "deletes his/her likes as well" do
       user_for_deletion = FactoryBot.create(:user)
       FactoryBot.create(:like, liker_id: user_for_deletion.id)
+      expect(Like.where(liker_id: user_for_deletion.id)).not_to be_empty
       user_for_deletion.destroy
 
       expect(Like.where(liker_id: user_for_deletion.id)).to be_empty
@@ -28,10 +30,11 @@ RSpec.describe "User-Like-Post associations", type: :model do
   context "deleting a post" do
     it "deletes it's likes as well" do
       post_for_deletion = FactoryBot.create(:post)
-      FactoryBot.create(:like, liked_post_id: post_for_deletion.id)
+      FactoryBot.create(:like, likeable_id: post_for_deletion.id, likeable_type: "Post")
+      expect(Like.where(likeable_id: post_for_deletion.id, likeable_type: "Post")).not_to be_empty
       post_for_deletion.destroy
 
-      expect(Like.where(liked_post_id: post_for_deletion.id)).to be_empty
+      expect(Like.where(likeable_id: post_for_deletion.id, likeable_type: "Post")).to be_empty
     end
   end
 end
