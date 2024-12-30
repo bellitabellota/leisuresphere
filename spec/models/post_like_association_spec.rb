402046c73e9b5e71f9e34ca91973f503470963dc
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-30 15:23:43 +0000

    Clean up Post-Like association test

--- spec/models/post_like_association_spec.rb
+++ spec/models/post_like_association_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Like-Post association", type: :model do
   # except for the dependent destroy the Like-Post association is well tested in:
-  # user_like_image_post_spec.rb
   # user.like_post_associations_spec.rb
   context "deleting a post" do
     it "deletes it's likes as well" do
-      post_for_deletion = FactoryBot.create(:post)
-      FactoryBot.create(:like, likeable_id: post_for_deletion.id, likeable_type: "Post")
-      expect(Like.where(likeable_id: post_for_deletion.id, likeable_type: "Post")).not_to be_empty
-      post_for_deletion.destroy
+      post = FactoryBot.create(:post)
+      FactoryBot.create(:like, likeable_id: post.id, likeable_type: "Post")
+      expect(Like.where(likeable_id: post.id, likeable_type: "Post")).not_to be_empty
+      post.destroy
 
-      expect(Like.where(likeable_id: post_for_deletion.id, likeable_type: "Post")).to be_empty
+      expect(Like.where(likeable_id: post.id, likeable_type: "Post")).to be_empty
     end
   end
 end
