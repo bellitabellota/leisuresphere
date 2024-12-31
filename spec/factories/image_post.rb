82654dd8d38887ecc32b667f9b509717ebfa821b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-31 11:32:32 +0000

    Update ImagePost factory to have body image attached

--- spec/factories/image_post.rb
+++ spec/factories/image_post.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :image_post do
     association :author, factory: :user
+
+    after(:build) do |image_post|
+      image_post.body.attach(
+        io: File.open(Rails.root.join("spec/support/assets/test_image_post_sunset.jpg")),
+        filename: "test_image_post_sunset.jpg",
+        content_type: "jpg"
+      )
+    end
   end
 end
