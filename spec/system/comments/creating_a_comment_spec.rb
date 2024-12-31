e293880e33e9723623e6e7b8cb39386ce07b4474
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-31 12:10:51 +0000

    Add test cases for image post to 'Creating a comment' test

--- spec/system/comments/creating_a_comment_spec.rb
+++ spec/system/comments/creating_a_comment_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe "Create a comment using button on post#index page", type: :system do
-  context "with valid input" do
+RSpec.describe "Create a comment using button on Feed page", type: :system do
+  context "comment on post with valid input" do
     it "new comment is displayed" do
       user = FactoryBot.create(:user)
       FactoryBot.create(:post, author_id: user.id)
@@ -15,10 +15,37 @@ RSpec.describe "Create a comment using button on post#index page", type: :system
     end
   end
 
-  context "with invalid input" do
+  context "comment on post with invalid input" do
     it "error message is displayed" do
       user = FactoryBot.create(:user)
       FactoryBot.create(:post, author_id: user.id)
+
+      login_as(user)
+      visit root_path
+      click_on "Add Comment"
+      click_on "Create Comment"
+      expect(page).to have_content("Body can't be blank")
+    end
+  end
+
+  context "comment on image post with valid input" do
+    it "new comment is displayed" do
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:image_post, author_id: user.id)
+
+      login_as(user)
+      visit root_path
+      click_on "Add Comment"
+      fill_in "comment_body", with: "This is a new comment."
+      click_on "Create Comment"
+      expect(page).to have_content("This is a new comment.")
+    end
+  end
+
+  context "comment on image post with invalid input" do
+    it "error message is displayed" do
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:image_post, author_id: user.id)
 
       login_as(user)
       visit root_path
