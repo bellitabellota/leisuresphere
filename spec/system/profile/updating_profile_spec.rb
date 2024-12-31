23fe8b2afb4ff290476082178189378d24aee60d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-31 14:37:56 +0000

    Add test case for upading profile with invalid image format

--- spec/system/profile/updating_profile_spec.rb
+++ spec/system/profile/updating_profile_spec.rb
@@ -1,20 +1,35 @@
 require "rails_helper"
 
 RSpec.describe "Update the user's profile", type: :system do
-  it "displays the profile page with the updated profile" do
-    user = FactoryBot.create(:user)
-    login_as(user)
-    visit edit_profile_path(user.profile)
+  context "with valid input" do
+    it "displays the profile page with the updated profile" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit edit_profile_path(user.profile)
 
-    fill_in "Birthday", with: "09/09/2009"
-    fill_in "Place of Residence", with: "Test Location"
-    fill_in "Interests", with: "Tests Interests"
-    attach_file "profile_profile_picture", Rails.root.join("spec/support/assets/test_image_post.png")
-    click_on "Update Profile"
+      fill_in "Birthday", with: "09/09/2009"
+      fill_in "Place of Residence", with: "Test Location"
+      fill_in "Interests", with: "Tests Interests"
+      attach_file "profile_profile_picture", Rails.root.join("spec/support/assets/test_image_post.png")
+      click_on "Update Profile"
 
-    expect(page).to have_css("img[src*='test_image_post.png']")
-    expect(page).to have_content("2009-09-09")
-    expect(page).to have_content("Test Location")
-    expect(page).to have_content("Tests Interests")
+      expect(page).to have_css("img[src*='test_image_post.png']")
+      expect(page).to have_content("2009-09-09")
+      expect(page).to have_content("Test Location")
+      expect(page).to have_content("Tests Interests")
+    end
+  end
+
+  context "with invalid image format" do
+    it "displays 'invalid content type' error message" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit edit_profile_path(user.profile)
+
+      attach_file "profile_profile_picture", Rails.root.join("spec/support/assets/invalid_image_format.gif")
+      click_on "Update Profile"
+
+      expect(page).to have_content("Profile picture has an invalid content type")
+    end
   end
 end
