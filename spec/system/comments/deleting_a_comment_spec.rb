fcf746850750189126a2284f71dd32683b85530a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-31 13:54:28 +0000

    Add test case for image post to 'Deleting a comment' test

--- spec/system/comments/deleting_a_comment_spec.rb
+++ spec/system/comments/deleting_a_comment_spec.rb
@@ -1,14 +1,29 @@
 require "rails_helper"
 
-RSpec.describe "Delete a comment using button on post#index page", type: :system do
-  it "comment is not displayed on post#index" do
-    user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post, author_id: user.id)
-    FactoryBot.create(:comment, commenter_id: user.id, commentable_id: post.id, commentable_type: "Post", body: "A comment waiting for deletion.")
+RSpec.describe "Delete a comment using button on Feed page", type: :system do
+  context "when deleting a comment of a post" do
+    it "comment is not displayed on Feed" do
+      user = FactoryBot.create(:user)
+      post = FactoryBot.create(:post, author_id: user.id)
+      FactoryBot.create(:comment, commenter_id: user.id, commentable_id: post.id, commentable_type: "Post", body: "A comment waiting for deletion.")
 
-    login_as(user)
-    visit root_path
-    click_on "Delete Comment"
-    expect(page).not_to have_content("A comment waiting for deletion.")
+      login_as(user)
+      visit root_path
+      click_on "Delete Comment"
+      expect(page).not_to have_content("A comment waiting for deletion.")
+    end
+  end
+
+  context "when deleting a comment of an image post" do
+    it "comment is not displayed on Feed" do
+      user = FactoryBot.create(:user)
+      image_post = FactoryBot.create(:image_post, author_id: user.id)
+      FactoryBot.create(:comment, commenter_id: user.id, commentable_id: image_post.id, commentable_type: "ImagePost", body: "A comment waiting for deletion.")
+
+      login_as(user)
+      visit root_path
+      click_on "Delete Comment"
+      expect(page).not_to have_content("A comment waiting for deletion.")
+    end
   end
 end
