84c68a36a5b154bb898d65fed157abe9512827c6
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-12-31 14:14:47 +0000

    Update Profile system test to check for updated profile picture as well

--- spec/system/profile/updating_profile_spec.rb
+++ spec/system/profile/updating_profile_spec.rb
@@ -9,8 +9,10 @@ RSpec.describe "Update the user's profile", type: :system do
     fill_in "Birthday", with: "09/09/2009"
     fill_in "Place of Residence", with: "Test Location"
     fill_in "Interests", with: "Tests Interests"
+    attach_file "profile_profile_picture", Rails.root.join("spec/support/assets/test_image_post.png")
     click_on "Update Profile"
 
+    expect(page).to have_css("img[src*='test_image_post.png']")
     expect(page).to have_content("2009-09-09")
     expect(page).to have_content("Test Location")
     expect(page).to have_content("Tests Interests")
