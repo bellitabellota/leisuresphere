cda9713fcc9d4e938959bf858da362ba07ad9f97
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-01-14 09:33:31 +0000

    Include profile in user query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id: current_user.id)
+    @users = User.includes(:profile).where.not(id: current_user.id)
     @followees = current_user.followees
   end
 
