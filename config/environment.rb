a834e7d6d9ad55afd04d6ad884e7b8efa8035763
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-01-15 10:01:17 +0000

    Debug mailer and sign in

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
   password: Rails.application.credentials.dig(:sendgrid_api_secret), # This is the secret sendgrid API key which was issued during API key creation
-  domain: "leisuresphere.fly.dev/",
+  domain: "leisuresphere-19144872.koyeb.app",
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
